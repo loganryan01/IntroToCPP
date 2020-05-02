e7b875c8be8be2578610424333f5afe4fffdc9ea
Logan Ryan <s200502@students.aie.edu.au>
2020-05-02 16:41:05 +1000

    Added game description,

--- TicTacToe/TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe/TicTacToe.cpp
@@ -13,15 +13,28 @@ int main()
     char player1Name[50];
     char player2Name[50];
     
-    cout << "Welcome to Tic-Tac-Toe" << endl;
+    // Print title of game
+    SetConsoleTextAttribute(h, 13 | FOREGROUND_INTENSITY);
+    cout << "\tWelcome to Tic-Tac-Toe" << endl;
+
+    // Print game description
+    SetConsoleTextAttribute(h, 15 | FOREGROUND_INTENSITY);
+    cout << "Tic-Tac-Toe is a game for two players who take turns marking the spaces in a 3x3 grid." << endl;
+    cout << "The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game." << endl;
+    
+    // Get player names
+    SetConsoleTextAttribute(h, 15 | FOREGROUND_INTENSITY);
     cout << "Player 1, What is your name? ";
     SetConsoleTextAttribute(h, 14 | FOREGROUND_INTENSITY);
     cin >> player1Name;
+    cout << endl;
     SetConsoleTextAttribute(h, 15 | FOREGROUND_INTENSITY);
     cout << "Player 2, What is your name? ";
     SetConsoleTextAttribute(h, 14 | FOREGROUND_INTENSITY);
     cin >> player2Name;
+    cout << endl;
 
+    // Print player names
     SetConsoleTextAttribute(h, 15 | FOREGROUND_INTENSITY);
     cout << "So we have " << player1Name << " vs. " << player2Name << endl;
     cout << "Let the game begin" << endl;
