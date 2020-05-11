6897da611dec0f786dbe3ce079d391fd832d4aa2
Logan Ryan <s200502@students.aie.edu.au>
2020-05-11 17:07:48 +1000

    Sorted code into functions.

--- PlayerProfileDatabase/PlayerProfileDatabase/PlayerProfileDatabase.cpp
+++ PlayerProfileDatabase/PlayerProfileDatabase/PlayerProfileDatabase.cpp
@@ -11,31 +11,91 @@ const char* TITLE = "\x1b[5;20H";
 const char* INDENT = "\x1b[5C";
 const char* YELLOW = "\x1b[93m";
 const char* MAGENTA = "\x1b[95m";
+const char* RED = "\x1b[91m";
+const char* BLUE = "\x1b[94m";
+const char* WHITE = "\x1b[97m";
+const char* GREEN = "\x1b[92m";
 const char* RESET_COLOR = "\x1b[0m";
-const char* SAVE_CURSOR_POS = "\x1b[s";
-const char* RESTORE_CURSOR_POS = "\x1b[u";
+
+const int EMPTY = 0;
+
+const int DISPLAY_WIDTH = 3;
+const int DISPLAY_HEIGHT = 10;
+
+const int INDENT_X = 5;
+const int DISPLAY_Y = 13;
+const int PLAYER_INPUT_X = 10;
+const int PLAYER_INPUT_Y = 9;
+
+bool enableVirtualTerminal();
+void initialize(int ladder[DISPLAY_HEIGHT][DISPLAY_WIDTH]);
+void drawWelcomeMessage();
+void drawRow(int ladder[DISPLAY_HEIGHT][DISPLAY_WIDTH], int x, int y);
+void drawHighScores(int ladder[DISPLAY_HEIGHT][DISPLAY_WIDTH]);
+void getScore();
+void getFirstLetter();
 
 int main()
 {
-    const int EMPTY = 0;
-    
-    const int DISPLAY_WIDTH = 3;
-    const int DISPLAY_HEIGHT = 10;
-
-    // Set output mode to handle virtual terminal sequences
-    DWORD dwMode = 0;
-    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    GetConsoleMode(hOut, &dwMode);
-    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-    SetConsoleMode(hOut, dwMode);
-
     // create a 2D array
     int ladder[DISPLAY_HEIGHT][DISPLAY_WIDTH];
 
-    int score = 0;
-    char firstLetterOfName = 0;
-    int position = 0;
+    bool gameOver = false;
+    if (enableVirtualTerminal() == false)
+    {
+        std::cout << "The virtual terminal processing mode could not be activated." << std::endl;
+        std::cout << "Press 'Enter' to exit." << std::endl;
+        std::cin.get();
+        return 0;
+    }
 
+    initialize(ladder);
+
+    drawWelcomeMessage();
+
+    // output the high-scores
+    drawHighScores(ladder);
+
+    while (!gameOver)
+    {
+        getScore();
+
+        getFirstLetter();
+    }
+
+    std::cout << CSI << PLAYER_INPUT_Y << ";" << 0 << "H";
+    std::cout << std::endl << INDENT << "Press 'Enter' to exit the program.";
+    std::cin.clear();
+    std::cin.ignore(std::cin.rdbuf()->in_avail());
+    std::cin.get();
+    return 0;
+} 
+
+bool enableVirtualTerminal()
+{
+    //Set output mode to handle virtual terminal sequences
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOut == INVALID_HANDLE_VALUE)
+    {
+        return false;
+    }
+
+    DWORD dwMode = 0;
+    if (!GetConsoleMode(hOut, &dwMode))
+    {
+        return false;
+    }
+    
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    if (!SetConsoleMode(hOut, dwMode))
+    {
+        return false;
+    }
+    return true;
+}
+
+void initialize(int ladder[DISPLAY_HEIGHT][DISPLAY_WIDTH])
+{
     // fill the arrays with 0
     for (int y = 0; y < DISPLAY_HEIGHT; y++)
     {
@@ -44,35 +104,52 @@ int main()
             ladder[y][x] = EMPTY;
         }
     }
+}
 
+void drawWelcomeMessage()
+{
     std::cout << TITLE << MAGENTA << "Welcome to the player profile database!" << RESET_COLOR << std::endl;
     std::cout << INDENT << "This database holds high-scores and who owns those high-scores." << std::endl;
     std::cout << INDENT << "It can be used for any game you wish." << std::endl << std::endl;
+}
 
-    std::cout << INDENT << "First, some questions..." << std::endl;
+void drawRow(int ladder[DISPLAY_HEIGHT][DISPLAY_WIDTH], int x, int y)
+{
+    // find the console output position
+    int outX = INDENT_X + (6 * x) + 1;
+    int outY = DISPLAY_Y + y;
 
-    // save cursor position
-    std::cout << SAVE_CURSOR_POS;
+    std::cout << CSI << outY << ";" << outX << "H";
+    // draw the room
+    switch (ladder[y][x])
+    {
+    case EMPTY:
+        std::cout << "[  ]";
+    }
+}
 
-    // output the high-scores
-    std::cout << std::endl;
-    std::cout << std::endl;
-    std::cout << std::endl;
-    std::cout << std::endl;
+void drawHighScores(int ladder[DISPLAY_HEIGHT][DISPLAY_WIDTH])
+{
+    // reset draw colors
+    std::cout << RESET_COLOR;
     for (int y = 0; y < DISPLAY_HEIGHT; y++)
     {
         std::cout << INDENT;
         for (int x = 0; x < DISPLAY_WIDTH; x++)
         {
-            std::cout << "[ " << ladder[y][x] << " ]";
+            drawRow(ladder, x, y);
         }
         std::cout << std::endl;
     }
+}
 
-    // move the cursor back to the top of the high-scores
-    std::cout << RESTORE_CURSOR_POS;
+void getScore()
+{
+    int score = 0;
+    
+    std::cout << CSI << PLAYER_INPUT_Y << ";" << 0 << "H";
+    std::cout << CSI << "2K";
     std::cout << INDENT << "What is your high-score? " << INDENT << YELLOW;
-
     std::cin >> score;
     std::cout << RESET_COLOR << std::endl;
     if (std::cin.fail())
@@ -88,18 +165,19 @@ int main()
     std::cin.clear();
     std::cin.ignore(std::cin.rdbuf()->in_avail());
     std::cin.get();
+    std::cout << CSI << "1A" << CSI << "2K";
+}
 
-    // delete line
-    std::cout << CSI << "1A";
-    std::cout << CSI << "0K";
-    // move the cursor to the start of the 1st question
-    std::cout << RESTORE_CURSOR_POS;
-    // delete the next 3 lines of text
-    std::cout << CSI << "0K";
+void getFirstLetter()
+{
+    char firstLetterOfName = 0;
+    
+    std::cout << CSI << PLAYER_INPUT_Y << ";" << 0 << "H";
+    std::cout << CSI << "2K";
     std::cout << INDENT << "What is the first letter of your name? " << INDENT << YELLOW;
-
     std::cin.clear();
     std::cin.ignore(std::cin.rdbuf()->in_avail());
+
     std::cin >> firstLetterOfName;
     std::cout << RESET_COLOR << std::endl;
 
@@ -114,31 +192,8 @@ int main()
     std::cin.clear();
     std::cin.ignore(std::cin.rdbuf()->in_avail());
     std::cin.get();
-
-    std::cout << CSI << "1A" << CSI << "0K";
-    // move the cursor to the start of the 1st question, then up 1, then delete and insert 4 lines
-    std::cout << RESTORE_CURSOR_POS << CSI << "A";
-
-    if (firstLetterOfName != 0)
-    {
-        position = (float)score / (firstLetterOfName * 0.02f);
-    }
-    else
-    {
-        position = 0;
-    }
-
-    std::cout << INDENT << "Using a complex deterministic algorithm, it has been calculated that you are "
-        << position << " in the world." << std::endl;
-
-    std::cout << CSI << "0K";
-
-    std::cout << std::endl << INDENT << "Press 'Enter' to exit the program.";
-    std::cin.clear();
-    std::cin.ignore(std::cin.rdbuf()->in_avail());
-    std::cin.get();
-    return 0;
-} 
+    std::cout << CSI << "1A" << CSI << "2K";
+}
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
